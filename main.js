e785b7c709c241e6e13a74a662236130bbd8af55
John <96757648+jwebdev1@users.noreply.github.com>
2023-08-09 11:02:15 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementsById("navLinks");
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+        }
+
+
